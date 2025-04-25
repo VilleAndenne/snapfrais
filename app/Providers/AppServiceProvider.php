a4359b97509f9bf124c5f746c1c1ccc3b25402b7
Sébastien Merveille <sebastienmerveilleriviere@gmail.com>
2025-04-25 15:08:12 +0200

    Register telescope if in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(ExpenseSheet::class, ExpenseSheetPolicy::class);
         Gate::policy(Department::class, DepartmentPolicy::class);
 
-        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+        if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
