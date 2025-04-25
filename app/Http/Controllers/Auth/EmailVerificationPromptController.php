13314c3192fb5886402f15d820da3c9966edeeee
Sébastien Merveille <sebastienmerveilleriviere@gmail.com>
2025-04-25 17:19:32 +0200

    directly send the verification mail when you arrive on VerifyMail view

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,8 +15,11 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|Response
     {
-        return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
-                    : Inertia::render('auth/VerifyEmail', ['status' => $request->session()->get('status')]);
+        if (!$request->user()->hasVerifiedEmail()) {
+            $request->user()->sendEmailVerificationNotification();
+            return Inertia::render('auth/VerifyEmail', ['status' => $request->session()->get('status')]);
+        }
+
+        return redirect()->intended(route('dashboard', absolute: false));
     }
 }
