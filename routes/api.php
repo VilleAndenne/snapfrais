cc4add2d715868a20d7787f8eabfba07d977535d
Sébastien Merveille <sebastienmerveilleriviere@gmail.com>
2025-04-28 12:15:01 +0200

    Fix auth by api

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [\App\Http\Controllers\API\Auth\AuthenticatedSessionController::class, 'store'])
-    ->name('api.login');
+Route::post('/login', [\App\Http\Controllers\API\Auth\Authcontroller::class, 'login'])
+    ->name('api.login')->middleware('api');
