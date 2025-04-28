628d83170c5f6ac25e50c46fd7d2f4868df5c1d2
Sébastien Merveille <sebastienmerveilleriviere@gmail.com>
2025-04-28 13:16:55 +0200

    AuthController update name

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [\App\Http\Controllers\API\Auth\Authcontroller::class, 'login'])
+Route::post('/login', [\App\Http\Controllers\API\Auth\AuthController::class, 'login'])
     ->name('api.login')->middleware('api');
